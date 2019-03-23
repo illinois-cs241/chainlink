76b852f685f78f6476ba493c884c1990f701d145
Ayush Ranjan <ayushr2@illinois.edu>
2019-03-23 04:23:54 -0500

    black reformat, updated git ignore for a python project, fixed imports and linter warnings

--- tests/integration/basic.py
+++ tests/integration/basic.py
@@ -3,33 +3,22 @@ import unittest
 from chainlink import Chainlink
 
 stages = [
-  {
-    "image": "alpine:3.5",
-    "entrypoint": ["env"],
-    "env": {
-      "ASSIGNMENT": "",
-      "NETID": "$NETID"
-    }
-  },
-  {
-    "image": "alpine:3.5",
-    "entrypoint": ["sleep", "2"]
-  }
+    {
+        "image": "alpine:3.5",
+        "entrypoint": ["env"],
+        "env": {"ASSIGNMENT": "", "NETID": "$NETID"},
+    },
+    {"image": "alpine:3.5", "entrypoint": ["sleep", "2"]},
 ]
-env = {
-  "TEST": "testing",
-  "SEMESTER": "sp18",
-  "ASSIGNMENT": "mp1"
-}
+env = {"TEST": "testing", "SEMESTER": "sp18", "ASSIGNMENT": "mp1"}
+
 
 class TestBasicChaining(unittest.TestCase):
+    def test_basic_chain(self):
+        chain = Chainlink(stages)
+        results = chain.run(env)
 
-  def test_basic_chain(self):
-    chain = Chainlink(stages)
-    results = chain.run(env)
-    
-    self.assertFalse(results[0]["killed"])
-    self.assertTrue("TEST=testing" in results[0]["logs"]["stdout"].decode("utf-8"))
-    self.assertFalse(results[0]["killed"])
-    self.assertEqual(results[1]["data"]["ExitCode"], 0)
-    
+        self.assertFalse(results[0]["killed"])
+        self.assertTrue("TEST=testing" in results[0]["logs"]["stdout"].decode("utf-8"))
+        self.assertFalse(results[0]["killed"])
+        self.assertEqual(results[1]["data"]["ExitCode"], 0)
--- tests/integration/privileged_test.py
+++ tests/integration/privileged_test.py
@@ -3,23 +3,23 @@ import unittest
 from chainlink import Chainlink
 
 stages = [
-  {
-    "image": "ubuntu:14.04",
-    "entrypoint": ["unshare", "--mount", "echo", "dumb"],
-    "privileged": True
-  },
-  {
-    "image": "ubuntu:14.04",
-    "entrypoint": ["ip", "link", "set", "lo", "up"],
-    "privileged": True
-  }
+    {
+        "image": "ubuntu:14.04",
+        "entrypoint": ["unshare", "--mount", "echo", "dumb"],
+        "privileged": True,
+    },
+    {
+        "image": "ubuntu:14.04",
+        "entrypoint": ["ip", "link", "set", "lo", "up"],
+        "privileged": True,
+    },
 ]
 
+
 class TestPrivileged(unittest.TestCase):
+    def test_basic_chain(self):
+        chain = Chainlink(stages)
+        results = chain.run({})
 
-  def test_basic_chain(self):
-    chain = Chainlink(stages)
-    results = chain.run({})
-
-    self.assertEqual(results[0]["data"]["ExitCode"], 0)
-    self.assertEqual(results[1]["data"]["ExitCode"], 0)
+        self.assertEqual(results[0]["data"]["ExitCode"], 0)
+        self.assertEqual(results[1]["data"]["ExitCode"], 0)
